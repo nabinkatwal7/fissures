df0a71ceb907f06973b6288174733a298ce09544
Nabin Katwal <91274113+nabinkatwal7@users.noreply.github.com>
2024-04-15 12:46:35 +0545

    ts and go

--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -11,3 +11,39 @@ function getInput(): string{
     5,5 -> 8,2`;
 }
 
+type Point = {
+    x: number; 
+    y: number;
+}
+
+type Line = {
+    p1: Point;
+    p2: Point;
+}
+
+function isHOrV(line:Line){
+    return line.p1.x === line.p2.x || line.p1.y === line.p2.y;
+}
+
+function parsePoint(p: string){
+    const [x,y] = p.split(",");
+    return {
+        x:+x,
+        y:+y,
+    }
+}
+
+function parseLine(line: string){
+    const [p1,p2] = line.split(" -> ");
+    return {
+        p1:parsePoint(p1),
+        p2:parsePoint(p2),
+    }
+}
+
+const lines = getInput().
+    split("\n").
+    map(x=>parseLine(x)).
+    filter(isHOrV)
+
+console.log(lines)
